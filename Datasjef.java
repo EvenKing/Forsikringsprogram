d9863431b8185cda7ce9d17206ae448ddfdb931f
Even <evva-gangsta@hotmail.com>
2018-03-11 23:25:18 +0100

    Create Datasjef.java

---
+++ Datasjef.java
@@ -0,0 +1,86 @@
+/*
+Henter fram og lagrer informasjon i programmet
+*/
+
+import java.io.*;
+
+public class Datasjef implements Serializable
+{
+	private static final long serialVersionUID = 42L;
+	StringBuilder lesSkrivError;
+	boolean error;
+	//Opprette collections i denne klassen kan løse NoClassDefFoundError
+
+	public Datasjef()
+	{
+		lesSkrivError = new StringBuilder(70);
+		error = false;
+	}
+
+	public Register lesRegisterFil()
+	{
+		// Leser fra fil.
+		Register register;
+		try (ObjectInputStream registerFil = new ObjectInputStream( new FileInputStream( "data/register.dtabse" ))){
+			register = (Register) registerFil.readObject();
+			register.setNåAnsattNr();
+			register.setNåKundeNr();
+			register.setNåForsikringsNr();
+			//register.setNåSkadeNr();
+			return register;
+		}
+		catch(ClassNotFoundException cnfe) {
+			System.out.println("Error: Cannot find class Register.\n");
+		    error = true;
+		}
+		catch(FileNotFoundException fne) {
+		    checkForFolder();
+		    register = new Register(this);
+		    skrivRegister(register);
+		    return register;
+		}
+		catch(IOException ioe) {
+			error = true;
+		   	lesSkrivError.append("Finner korrupt data hos registret. Slett register.dtabse for å løse problemet.\n");
+		}
+		return null;
+	}
+
+
+	public void skrivRegister(Register registerPara)
+	{
+		// Skriver til fil
+		try (ObjectOutputStream skrivRegister = new ObjectOutputStream(new FileOutputStream("data/register.dtabse"))) {
+		      skrivRegister.writeObject(registerPara);
+		}
+		catch( NotSerializableException nse ) {
+			error = true;
+		    lesSkrivError.append("Registeret er ikke serialisert.\n\n");
+		}
+		catch( IOException ioe ) {
+			error = true;
+		    lesSkrivError.append("Feil ved skriving av filen register.dtabse.\n\n");
+		}
+	}
+
+	public void checkForFolder()
+	{
+		// Sjekker om data mappe eksisterer
+		File checkFolder = new File("data");
+		if(!checkFolder.exists())
+		{
+			checkFolder.mkdir();
+		}
+	}
+
+	public String errorMessage()
+	{
+		error = false;
+		return lesSkrivError.toString();
+	}
+
+	public boolean getError()
+	{
+		return error;
+	}
+}
