eaacb8d2f7c825a65e3375c129eb1e3f33c9db00
Even <evva-gangsta@hotmail.com>
2018-03-11 23:39:02 +0100

    Create Statistikk.java

---
+++ Statistikk.java
@@ -0,0 +1,18 @@
+/*
+Tegner statistikk for:
+
+-skademeldinger per kunde
+-skademeldinger total
+-inntekter
+-utgifter
+-antall kunder
+-antall forsikringer total
+-antall forsikringer av forskjellige typer
+-
+
+
+*/
+import java.util.*;
+import javax.swing.table.*;
+
+public class Statistikk
