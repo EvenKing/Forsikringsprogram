22f0a79b22222cda9de48bcebb21bf277a71daf9
Even <evva-gangsta@hotmail.com>
2018-03-11 23:28:38 +0100

    Create ForsikringsReg.java

---
+++ ForsikringsReg.java
@@ -0,0 +1,62 @@
+/*
+Denne klassen er bare til for å skille mellom forsikringene
+*/
+
+import java.io.Serializable;
+import java.util.*;
+import javax.swing.*;
+
+public class ForsikringsReg implements Serializable
+{
+	private static final long serialVersionUID = 42L;
+	private Forsikring1 f;
+	private BilForsikringsReg bil;
+	private BåtForsikringsReg båt;
+	private HusForsikringsReg hus;
+	private HytteForsikringsReg hyt;
+
+	public ForsikringsReg()
+	{
+		bil = new BilForsikringsReg();
+		båt = new BåtForsikringsReg();
+		hus = new HusForsikringsReg();
+		hyt = new HytteForsikringsReg();
+	}
+
+	public int size()
+	{
+		return bil.size() + båt.size() + hus.size() + hyt.size();
+	}
+
+	public int sjekkForsikringsNr(int nr) //Gjør om til switch-metode
+	{
+		if(nr >= f.bilKat)
+		{
+			if(nr >= f.båtKat)
+			{
+				if(nr >= f.husKat)
+				{
+					if(nr >= f.hytKat)
+					{
+						if(nr > f.hytKat)
+						{
+							return 0;
+						}
+					}
+					else
+						return f.TYPE_HYTTE;
+				}
+				else
+					return f.TYPE_HUS;
+			}
+			else
+				return f.TYPE_BÅT;
+		}
+		else
+			return f.TYPE_BIL;
+
+		return 0;
+	}
+
+
+}
